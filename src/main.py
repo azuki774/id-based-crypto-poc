5cdfe9df0c28fa0019755b44015699014ec1f104
azuki774 <azuki774s@gmail.com>
2022-06-05 16:32:53 +0900

    add server and client

--- src/main.py
+++ src/main.py
@@ -1,15 +1,88 @@
+from flask import Flask, jsonify, request
 from pycocks.cocks.cocks import CocksPKG, Cocks
+from gmpy2 import mpz
 import pickle
+import json
+import sys
+import os
+import datetime
+
+args = sys.argv
 
 cocks_pkg = CocksPKG()  # Optional param.: bit size (default = 2048)
-
-# Extract private key, r, from an identity string; a transformed
-# ID string, a, is also returned, which is required for encryption
-# and decryption.
-r, a = cocks_pkg.extract("User1")
-
 cocks = Cocks(cocks_pkg.n)  # Must use same public modulus, n, from cocks_pkg
-c = cocks.encrypt(b"test", a)
+app = Flask(__name__)
 
-msg = cocks.decrypt(c, r, a)  # => b"test"
-print(a)
+UserDic = {}
+
+BASE_DIR = os.getcwd()
+MESSAGE_DIR = BASE_DIR + "/data/message"
+
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+
+@app.route("/users", methods=["POST"])
+def createUser():
+    id = request.get_json()["id"]
+    r, a = cocks_pkg.extract(id)
+    res = {"id": id, "private_key": int(r), "public_key": int(a)}
+    UserDic[id] = {"private_key": int(r), "public_key": int(a)}
+    return jsonify(res)
+
+
+@app.route("/users", methods=["GET"])
+def showUsers():
+    return jsonify(UserDic)
+
+
+@app.route("/message/encrypt", methods=["POST"])
+def createMessage():
+    public_key = request.get_json()["public_key"]
+    message = request.get_json()["message"]
+    c = encrypt(message, int(public_key))
+
+    dt_now = datetime.datetime.now()
+    filename = "mes" + dt_now.strftime("%H%M%S")
+    with open(MESSAGE_DIR + "/" + filename, "wb") as binfile:
+        pickle.dump(c, binfile)
+
+    return "saved as " + filename
+
+
+@app.route("/message/decrypt", methods=["POST"])
+def decryptMessage():
+    public_key = request.get_json()["public_key"]
+    private_key = request.get_json()["private_key"]
+    crypto_message_file = request.get_json()["crypto_file"]
+    with open(MESSAGE_DIR + "/" + crypto_message_file, "rb") as binfile:
+        crypto_message = pickle.load(binfile)
+    ptext = decrypt(crypto_message, private_key, public_key)
+
+    return ptext
+
+
+#######################################
+
+
+def encrypt(message, a):
+    mesbin = message.encode("utf-8")
+    c = cocks.encrypt(mesbin, mpz(a))
+    return c
+
+
+def decrypt(c, r, a):
+    original_message = cocks.decrypt(c, r, a)
+    return original_message.decode("utf-8")
+
+
+def server_main():
+    app.run(debug=True, host="0.0.0.0", port=12300)
+
+
+if __name__ == "__main__":
+    print("server start")
+    print(MESSAGE_DIR)
+    server_main()
